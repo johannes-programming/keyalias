ba9a38c0e953a9dcf9bbcd03cb0622715e8b8ea6
Johannes <johannes-programming@mailfence.com>
2024-09-24 11:14:17 +0200

    Version 1.0.0

--- src/keyalias/__init__.py
+++ src/keyalias/__init__.py
@@ -1,6 +1,34 @@
-def main(args=None):
-    print("Hello World!")
+import functools
+
+from tofunc import tofunc
+
+__all__ = ["keyalias"]
 
 
-if __name__ == "__main__":
-    main()
+def keyalias(**kwargs):
+    return functools.partial(decorator, **kwargs)
+
+
+def propertyget(self, /, *, key):
+    return self[key]
+
+
+def propertyset(self, value, /, *, key):
+    self[key] = value
+
+
+def propertydel(self, /, *, key):
+    del self[key]
+
+
+def decorator(cls, /, **kwargs):
+    raws = [propertyget, propertyset, propertydel]
+    for alias, key in kwargs.items():
+        bindings = list()
+        for raw in raws:
+            b = functools.partial(raw, key=key)
+            b = tofunc(b)
+            bindings.append(b)
+        pro = property(*bindings)
+        setattr(cls, alias, pro)
+    return cls
