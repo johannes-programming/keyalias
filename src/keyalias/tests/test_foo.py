3709fb321da883ca3822f2bb96179510c837955a
Johannes <johannes-programming@mailfence.com>
2025-02-08 23:32:17 +0100

    Version 1.0.4

--- src/keyalias/tests/test_foo.py
+++ src/keyalias/tests/test_foo.py
@@ -1,26 +1,27 @@
 import unittest
+from typing import *
 
 from keyalias import keyalias
 
 
 class TestFoo(unittest.TestCase):
-    def test_foo(self):
+    def test_foo(self: Self):
         @keyalias(bar=6)
         class Foo:
-            def __init__(self):
+            def __init__(self: Self) -> None:
                 self.data = list(range(10))  # Example list with 10 elements
 
-            def __getitem__(self, index):
+            def __getitem__(self: Self, index: Any) -> Any:
                 return self.data[index]
 
-            def __setitem__(self, index, value):
+            def __setitem__(self: Self, index: Any, value: Any) -> None:
                 self.data[index] = value
 
-            def __delitem__(self, index):
+            def __delitem__(self: Self, index: Any) -> None:
                 del self.data[index]
 
         # Create an instance of Foo
-        my_instance = Foo()
+        my_instance: Foo = Foo()
 
         # Use the alias property to access index 6
         self.assertEqual(my_instance.bar, 6)  # Get the value at index 6
